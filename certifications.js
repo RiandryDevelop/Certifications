88ec0da34e0f9170f10f84a549db8a2f4cfce714
RiandryDeveloper <riandryconnordeleon@gmail.com>
2024-04-26 19:40:56 -0400

    Refactor(Codebase): Add new certification -Re-design the images layout  -Re-write code text

--- certifications.js
+++ certifications.js
@@ -3,8 +3,8 @@ const $button_1 = document.getElementById("certification_1"),
   $button_2 = document.getElementById("certification_2"),
   $button_3 = document.getElementById("certification_3"),
   $button_4 = document.getElementById("certification_4"),
-  $button_5 = document.getElementById("certification_3");
-
+  $button_5 = document.getElementById("certification_5"),
+  $button_6 = document.getElementById("certification_6");
 // now we adding a event to the button and open it the respectivy page
 
 $button_1.addEventListener("click", () => {
@@ -15,7 +15,7 @@ $button_1.addEventListener("click", () => {
 
 $button_2.addEventListener("click", () => {
   window.open(
-    "https://www.freecodecamp.org/certification/RiandryConnor/javascript-algorithms-and-data-structures"
+    "https://www.freecodecamp.org/certification/RiandryConnor/javascript-algorithms-and-data-structures-v8"
   );
 });
 $button_3.addEventListener("click", () => {
@@ -29,5 +29,9 @@ $button_4.addEventListener("click", () => {
   );
 });
 $button_5.addEventListener("click", () => {
-  window.open("#");
+  window.open("https://www.freecodecamp.org/certification/RiandryConnor/back-end-development-and-apis");
+});
+
+$button_6.addEventListener("click", () => {
+  window.open("https://www.freecodecamp.org/certification/RiandryConnor/javascript-algorithms-and-data-structures");
 });
